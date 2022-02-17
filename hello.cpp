996b8e849980758ca2ac1ce7b2c79f780d7a15b4
Greg-Chinnici <91907134+Greg-Chinnici@users.noreply.github.com>
2022-02-16 19:12:26 -0800

    Delete hello.cpp

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-/// @file <hello.cpp>
-/// @brief <prints a Hello C++!>
-/// @author chinnici@chapman.edu
-#include <iostream>
-int main()
-{
-    std::cout << "Hello C++!" << std::endl;
-    return 0;
-}
